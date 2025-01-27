6c2842fa15cb8bc645a6557b22abf934fc6239bb
mdspenc1 <mdspenc1@ualberta.ca>
2025-01-27 15:35:56 -0700

    Add files via upload

---
+++ AdhesionTest/MainViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace AdhesionTest
+{
+    public class MainViewModel
+    {
+        private const string NORMTITLE = "Normal Force";
+        private const string SHEARTITLE = "Shear Force";
+        private const int TIMEHISTORY = 600; //The amount of time history that should be kept in the graph
+
+        public MainViewModel()
+        {
+            smoothData = false;
+
+            normalPlot = new PlotModel();
+            normalPlot.Series.Add(new LineSeries());
+            normalPlot.Title = NORMTITLE;
+            normalPlot.Axes.Add(new LinearAxis {Position = AxisPosition.Bottom, Title = "Time Elapsed (seconds)"});
+            normalPlot.Axes.Add(new LinearAxis {Position = AxisPosition.Left, Title = "Force (mN)"});
+            normalPlot.DefaultColors = new List<OxyColor>
+            {
+                OxyColor.FromRgb(30, 144, 255)
+            };
+
+            shearPlot = new PlotModel();
+            shearPlot.Series.Add(new LineSeries());
+            shearPlot.DefaultColors = new List<OxyColor>
+            {
+                OxyColor.FromRgb(30, 144, 255)
+            };
+            shearPlot.Title = SHEARTITLE;
+            shearPlot.Axes.Add(new LinearAxis {Position = AxisPosition.Bottom, Title = "Time Elapsed (seconds)"});
+            shearPlot.Axes.Add(new LinearAxis {Position = AxisPosition.Left, Title = "Force (mN)"});
+
+            timerStart = DateTime.Now;
+        }
+
+        private static DateTime timerStart { get; set; }
+
+        public static PlotModel normalPlot { get; set; }
+        public static PlotModel shearPlot { get; set; }
+
+        public static Label normalForceLabel { get; set; }
+        public static Label shearForceLabel { get; set; }
+
+        public static bool smoothData { get; set; }
+        public static DAQ dataAcquirer { get; private set; }
+
+        public static void setDataAcquirer(DAQ value)
+        {
+            dataAcquirer = value;
+            dataAcquirer.dataAcquiredEvent += updateGraph;
+        }
+
+        private static void updateGraph(object sender, EventArgs e)
+        {
+            if (dataAcquirer.dataPoints.Count > 0)
+            {
+                var time = (DateTime.Now - timerStart).TotalMilliseconds;
+                time /= 1000;
+                (normalPlot.Series[0] as LineSeries).Points.Add(new DataPoint(time,
+                    dataAcquirer.dataPoints[dataAcquirer.dataPoints.Count - 1].normalForce));
+                //If there are too many elements, remove the first
+                if ((normalPlot.Series[0] as LineSeries).Points.Count*Constants.DAQFREQ > TIMEHISTORY)
+                {
+                    (normalPlot.Series[0] as LineSeries).Points.RemoveAt(0);
+                }
+
+                (normalPlot.Series[0] as LineSeries).Smooth = smoothData;
+                normalPlot.InvalidatePlot(true);
+
+                (shearPlot.Series[0] as LineSeries).Points.Add(new DataPoint(time,
+                    dataAcquirer.dataPoints[dataAcquirer.dataPoints.Count - 1].shearForce));
+
+                //If there are too many elements, remove the first
+                if ((shearPlot.Series[0] as LineSeries).Points.Count*Constants.DAQFREQ > TIMEHISTORY)
+                {
+                    (shearPlot.Series[0] as LineSeries).Points.RemoveAt(0);
+                }
+
+                (shearPlot.Series[0] as LineSeries).Smooth = smoothData;
+                shearPlot.InvalidatePlot(true);
+
+                normalForceLabel.Content =
+                    dataAcquirer.dataPoints[dataAcquirer.dataPoints.Count - 1].normalForce.ToString("0.000");
+                shearForceLabel.Content =
+                    dataAcquirer.dataPoints[dataAcquirer.dataPoints.Count - 1].shearForce.ToString("0.000");
+            }
+            else
+            {
+                Console.WriteLine("NO DATA POINTS");
+            }
+        }
+    }
+}
---
+++ AdhesionTest/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using AdhesionTest.Properties;
+using MahApps.Metro.Controls;
+using System.IO;
+using System.Reflection;
+
+namespace AdhesionTest
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public static Frame mainFrame;
+
+        public delegate void errorThrown(object sender, EventArgs e);
+        public static event errorThrown errorThrownEvent;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //update flyout settings
+            comPortBox.Text = Settings.Default.motorControllerPort.ToString();
+            normalIdBox.Text = Settings.Default.normalChannelId;
+            shearIdBox.Text = Settings.Default.shearChannelId;
+
+            mainFrame = MyFrame;
+            mainFrame.Navigate(new calibration());
+            settingsFlyout.ClosingFinished += flyoutClosed;
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(ErrorHandler);
+        }
+
+        static void ErrorHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            Console.WriteLine("ERROR log saved");
+            string m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            MessageBox.Show("An error has occurred. The error log is located at: " + m_exePath);
+            string specifier = getSpecifier(m_exePath + "\\" + "log", ".txt");
+            using (StreamWriter writer = File.AppendText(m_exePath + "\\" + "log" + specifier + ".txt"))
+            {
+                Exception ex = (Exception)args.ExceptionObject;
+                writer.WriteLine("Message :" + ex.Message + Environment.NewLine + ex.TargetSite + Environment.NewLine + "DATA" + ex.Data + Environment.NewLine + "INNER EXCEPTION: " + ex.InnerException + Environment.NewLine + "SOURCE" + ex.Source + Environment.NewLine + "TARGET SITE:" + ex.TargetSite + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
+  "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+            }
+            errorThrownEvent(typeof(MainWindow), new EventArgs());
+
+        }
+
+        /// <summary>
+        ///     returns the specifier needed at the end of the file -1, -2 ...
+        /// </summary>
+        /// <param name="filePath"> the file path, including the file itself, but not the extension </param>
+        /// <param name="fileExtension"> the file extension </param>
+        /// <returns></returns>
+        private static string getSpecifier(string filePath, string fileExtension)
+        {
+            if (File.Exists(filePath + fileExtension))
+            {
+                Console.WriteLine("File exists already");
+                var currentSpecifier = 1;
+                while (File.Exists(filePath + "-" + currentSpecifier + fileExtension))
+                {
+                    currentSpecifier++;
+                }
+                return "-" + currentSpecifier;
+            }
+            return ""; //no specifier needed, don't return one
+        }
+
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
+            settingsFlyout.IsOpen = true;
+        }
+
+        //The flyout has closed, save its settings
+        private void flyoutClosed(object sender, EventArgs e)
+        {
+            var update = false;
+            if (normalIdBox.Text != Settings.Default.normalChannelId ||
+                shearIdBox.Text != Settings.Default.shearChannelId)
+            {
+                update = true;
+            }
+
+            Settings.Default.motorControllerPort = Convert.ToInt32(comPortBox.Text);
+            Settings.Default.normalChannelId = normalIdBox.Text;
+            Settings.Default.shearChannelId = shearIdBox.Text;
+
+            Settings.Default.Save();
+
+            if (update)
+            {
+                var dataAcquirer = new DAQ(Settings.Default.normalChannelId, Settings.Default.shearChannelId);
+                MainViewModel.setDataAcquirer(dataAcquirer);
+                mainFrame.Navigate(new calibration());
+            }
+
+            Console.WriteLine(Settings.Default.shearChannelId + Settings.Default.normalChannelId);
+        }
+
+        private void TextBoxPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!sanitizeInput(e.Text))
+            {
+                //Invalid input
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        ///     Ensures that only numbers
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns>Returns true if valid</returns>
+        private bool sanitizeInput(string inputString)
+        {
+            if (!char.IsDigit(inputString, inputString.Length - 1))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
